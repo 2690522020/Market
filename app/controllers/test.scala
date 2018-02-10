cc8f64c1c9f4397019284eab6a13ab1f9d12ca7d
zjw <2690522020@qq.com>
2018-02-10 14:02:33 +0800

    add wechat and basici routes file

---
+++ app/controllers/test.scala
@@ -0,0 +1,61 @@
+package controllers
+import akka.actor.{ Actor, ActorLogging, Props ,ActorSystem}
+import scala.io.StdIn
+/**
+  * Created by zjw on 2018/02/09.
+  * this Is test
+  */
+
+object IotSupervisor {
+  def props(): Props = Props(new IotSupervisor)
+}
+
+class IotSupervisor extends Actor with ActorLogging {
+  override def preStart(): Unit = log.info("IoT Application started")
+  override def postStop(): Unit = log.info("IoT Application stopped")
+
+  // No need to handle any messages
+  override def receive = Actor.emptyBehavior
+
+}
+object IotApp {
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("iot-system")
+
+    try {
+      // Create top level supervisor
+      val supervisor = system.actorOf(IotSupervisor.props(), "iot-supervisor")
+      // Exit the system after ENTER is pressed
+      StdIn.readLine()
+    } finally {
+      system.terminate()
+    }
+  }
+
+}
+
+final case class ReadTemperature(requestId: Long)
+final case class RespondTemperature(requestId: Long, value: Option[Double])
+
+object Device {
+  def props(groupId: String, deviceId: String): Props = Props(new Device(groupId, deviceId))
+
+  final case class ReadTemperature(requestId: Long)
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+}
+
+class Device(groupId: String, deviceId: String) extends Actor with ActorLogging {
+  import Device._
+
+  var lastTemperatureReading: Option[Double] = None
+
+  override def preStart(): Unit = log.info("Device actor {}-{} started", groupId, deviceId)
+  override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
+
+  override def receive: Receive = {
+    case ReadTemperature(id) ⇒
+      sender() ! RespondTemperature(id, lastTemperatureReading)
+  }
+
+}
